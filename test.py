3c04022352827c8731fd334054cb8c772cf7b584
RiekCyber <Rikeseb@gmail.mail>
2021-11-16 19:45:35 +0100

    Git Commit - Done.

---
+++ test.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+HOST = 'sandbox-iosxe-recomm-1.cisco.com'
+USER = 'developer'
+PASS = 'C1sco12345'
+
+url = "https://sandbox-iosxe-recomm-1.cisco.com:443/restconf/data/ietf-interfaces:interfaces"
+headers = {'Content-Type': 'application/yang-data+json','Accept': 'application/yang-data+json'}
+
+response = requests.get(url, auth=(USER, PASS), headers=headers, verify=False)
+
+print(response.content)
+#interfaces = response.json()
+#print(interfaces['ietf-interfaces:interfaces']['interface'][0]['name'])
+#print(interfaces['ietf-interfaces:interfaces']['interface'][0]['ietf-ip:ipv4']['address'][0]['ip'])
